05f551df5557799b48aa3825b703fc58b9f0000b
swati <91478967+swati-kaithwas@users.noreply.github.com>
2021-12-13 16:07:20 +0530

    6

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 
 const { register, login } = require("./controllers/auth.controller");
-const productController = require("./controllers/product.controller");
+const movieController = require("./controllers/movie.controller");
+const screenController = require("./controllers/screen.controller");
+const seatController = require("./controllers/seat.controller");
+const showController = require("./controllers/show.controller");
+const theatreController = require("./controllers/threatres.controller");
 
 const passport = require("./configs/passport");
 
@@ -22,7 +26,11 @@ passport.deserializeUser(function (user, done) {
 app.post("/register", register);
 app.post("/login", login);
 
-app.use("/products", productController);
+app.use("/movies", movieController);
+app.use("/seats", seatController);
+app.use("/shows", showController);
+app.use("/theatres", threatreController);
+app.use("/screens", screenController);
 
 app.get(
   "/auth/google",
